87c7650153190dea4371e357aed467ece4118f26
“yukezheng” <zhengyk19@gmail.com>
2025-05-04 14:26:21 -0400

    change default epcochs

--- train.py
+++ train.py
@@ -40,7 +40,7 @@ def get_args():
                        help='Theta parameter for RoPE variants (lower value = higher frequency)')
     # Training hyperparameters
     parser.add_argument('--batch_size', type=int, default=128)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=30)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--weight_decay', type=float, default=0.01)
     # Model architecture parameters
